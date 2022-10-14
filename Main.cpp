d69da99468910e486116c36ee022ebea51f81585
Matthew Holmes <matthew.holmes111@gmail.com>
2022-10-14 15:11:20 +0100

    Tidied up (previous commments / layout in preTidy branch)

--- Main.cpp
+++ Main.cpp
@@ -51,56 +51,51 @@ int main() {
 
 	Shader shaderProgram("default.vert", "default.frag");
 
-	
 	// VBO - vertex buffer object: stores data in GPU memory 
 	// VAO - vertex array object: stores pointers to one or more VBOs
-	GLuint VAO, VBO, EBO;
+	// Generates Vertex Array Object and binds it
+	VAO VAO1;
+	VAO1.Bind();
 
-	glGenVertexArrays(1, &VAO); // must generate VAO before VBO
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
 
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// store our vertices in the VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// make our element array current, aka - bind it	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// then introduce the indces
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// configure the VAO so openGL knows how to read the VBOs it stores
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// unbind both the VAO and VBO so a function can't accidentally change them 
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // bind to zero not what we had before
-	glBindVertexArray(0); // order matters 
-	// Bind the EBO to 0 so that we don't accidentally modify it
-	// MAKE SURE TO UNBIND IT AFTER UNBINDING THE VAO, as the EBO is linked in the VAO
-	// This does not apply to the VBO because the VBO is already linked to the VAO during glVertexAttribPointer
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	// Links VBO to VAO
+	VAO1.LinkVBO(VBO1, 0);
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
 
 	// loop until the window is closed
 	while (!glfwWindowShouldClose(window)) {
+		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
-		glBindVertexArray(VAO);
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
-		glfwSwapBuffers(window); 
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VBO);
-	glDeleteBuffers(1, &VAO);
-	glDeleteBuffers(1, &EBO);
+	// Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
 	shaderProgram.Delete();
-
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
