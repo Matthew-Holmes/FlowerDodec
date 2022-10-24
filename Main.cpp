32b514cc8a35aae3371e1d1a4c50ab0a2e83b6b0
Matthew Holmes <matthew.holmes111@gmail.com>
2022-10-24 16:42:34 +0100

    tuning, tidying and comments

--- Main.cpp
+++ Main.cpp
@@ -24,20 +24,16 @@ GLfloat x_vel = 0.01f, y_vel = 0.008f;
 GLfloat x_pos = 0.0f, y_pos = 0.0f;
 GLfloat x_max = width / (zoom * 700.0f), y_max = height / (zoom * 700.0f);
 
-const GLfloat spread = 2.0f; // how extreme we interpolate
-							 // 1.0f corresponds to original 
-
-
-
+const GLfloat spread = 1.60; // how extreme we interpolate
+							 // 1.0f corresponds to original
 
 int main() {
+	// *********   init   ********************
 	glfwInit();
-
 	// tell glfw what version of openGL we are using
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 	// create the window
 	GLFWwindow* window = glfwCreateWindow(width, height, "FlowerDodec", NULL, NULL);
 	if (window == NULL) {
@@ -47,34 +43,31 @@ int main() {
 	}
 	// now tell glfw to use the window we just created
 	glfwMakeContextCurrent(window);
-
 	// have glad load the needed configurations for openGl
 	gladLoadGL();
-
 	// tell openGL the area of the window we want it to render in
 	glViewport(0, 0, width, height);
-
 	Shader shaderProgram("default.vert", "default.frag");
+
+	// **********  compute geometry  **************************
+
+	//SurfaceSquare surface(-1.0f, -1.0f, 1.0f, 1.0f, 50, 50);
+	SurfacePentagon surface(4);
+	surface.generate();
+	FaceDataGenerator facedatgen;
+	std::vector<FaceData> dodecFaceData = facedatgen.genDodecData();
+
+	// *************** fill buffers *********************************
 
 	// VBO - vertex buffer object: stores data in GPU memory 
 	// VAO - vertex array object: stores pointers to one or more VBOs
 	// Generates Vertex Array Object and binds it
 	VAO VAO1;
 	VAO1.Bind();
-
-	// SurfaceSquare surface(-1.0f, -1.0f, 1.0f, 1.0f, 50, 50);
-	SurfacePentagon surface(4);
-	surface.generate();
-	FaceDataGenerator facedatgen;
-	std::vector<FaceData> dodecFaceData = facedatgen.genSimpleData();
-
-
-	// TODO update VBO to work with vectors
 	// Generates Vertex Buffer Object and links it to vertices
 	VBO VBO1(&surface.vertices[0], surface.vertices.size() * sizeof(GLfloat));
 	// Generates Element Buffer Object and links it to indices
 	EBO EBO1(&surface.indices[0], surface.indices.size() * sizeof(GLuint));
-
 	// Links VBO to VAO
 	// link attributes for coordinates
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0 );
@@ -85,14 +78,12 @@ int main() {
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	float rotation = 0.0f;
+	// ********** prepare to draw *****************************
+
+	float rotation = 0.0f; // rotation angle of shape
 	GLfloat prevTime = glfwGetTime();
 
-	// generate the data about the cube faces
-
-
-
-	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST); // so only see frontmost geometry
 
 	// initialise the matrices we'll need
 	glm::mat4 model = glm::mat4(1.0f); // leave as is since model in the centre
@@ -101,6 +92,7 @@ int main() {
 	glm::mat4 proj = glm::mat4(1.0f);
 	glm::mat4 ortn = glm::mat4(1.0f); // orientation matrix
 
+	// camera and world settings
 	// move the world down 0.0 and forward 4.0
 	glm::vec3 camPos(0.0f, 0.0f, 4.0f);
 	view = glm::translate(view, -camPos);
@@ -113,6 +105,9 @@ int main() {
 	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 	glm::vec3 lightPos = glm::vec3(5.0f, 5.0f, 5.0f); // relative to shape
 
+
+	// ************************ main loop *******************************
+	
 	// loop until the window is closed
 	while (!glfwWindowShouldClose(window)) {
 		// Specify the color of the background
@@ -121,10 +116,12 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
-
 		GLfloat crntTime = glfwGetTime();
+
+		// **************** frame update *************************
 		if (crntTime - prevTime >= 1.0f / 60.0f)
 		{
+			// update position and rotation of shape
 			rotation += 0.5f;
 			x_pos += x_vel;
 			y_pos += y_vel;
@@ -134,12 +131,15 @@ int main() {
 				y_vel *= -1.0f;
 			prevTime = crntTime;
 			
+			// translate and rotate the model
 			trans = glm::translate(glm::mat4(1.0f), glm::vec3(x_pos, y_pos, 0.0f));
 			model = glm::rotate(glm::mat4(1.0f), glm::radians(rotation), glm::vec3(1.0f, 1.0f, 1.0f));
 
+			// link the uniforms
 			int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
 			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
+			// matrix uniforms
 			int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
 			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
@@ -152,11 +152,13 @@ int main() {
 			int transLoc = glGetUniformLocation(shaderProgram.ID, "trans");
 			glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
+			// morph uniform
 			int morphLoc = glGetUniformLocation(shaderProgram.ID, "morph");
-			glUniform1f(morphLoc, spread * (4.5 - std::abs(1.6 * std::fmod(prevTime / spread, 7.5f) - 6)));
+			glUniform1f(morphLoc, spread * (5.33f - std::abs(1.6 * std::fmod(prevTime / spread, 7.5f) - 6)));
 			// glUniform1f(morphLoc, 0.0f); // no morph
-			int colorLoc = glGetUniformLocation(shaderProgram.ID, "color");
 
+			// uniforms for colors and lighting computations
+			int colorLoc = glGetUniformLocation(shaderProgram.ID, "color");
 			glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"),
 				lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 			glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"),
@@ -164,10 +166,10 @@ int main() {
 			glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"),
 				camPos.x, camPos.y, camPos.z);
 
-
-
 			// Bind the VAO so OpenGL knows to use it
 			VAO1.Bind();
+			
+			// ********************* draw geometry *****************************
 			// Draw primitives, number of indices, datatype of indices, index of indices
 			// generalise so we don't need to keep track of the number of indices
 			for (FaceData face : dodecFaceData) {
@@ -183,6 +185,8 @@ int main() {
 		// Take care of all GLFW events
 		glfwPollEvents();
 	}
+
+	// **************** shutdown **********************
 
 	// Delete all the objects we've created
 	VAO1.Delete();
